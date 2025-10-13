156d59f4c2dc8253f77d0647cc00a2d683bff4d2
Ansh <saxenaansh27@gmail.com>
2025-10-13 13:12:19 +0530

    Category.java added annotations CategoryController.java added valid annotation CategoryRepository.java added ORM JPA CategoryServiceImpl.java Optimized the code of Update method MyGlobalExceptionHandler.java added this to handle all the global exceptions

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,45 +1,24 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "categories")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
+    @NotBlank
+    @Size(min=5, message = "Name must be contain 5 or more then 5 characters")
     private String categoryName;
 
-
-public Category(){}
-
-
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    @Override
-    public String toString() {
-        return "Category{" +
-                "categoryId=" + categoryId +
-                ", categoryName='" + categoryName + '\'' +
-                '}';
-    }
 
 }
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,45 +15,39 @@ import java.util.Optional;
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    private List<Category> categories=new ArrayList<>();
-    private Long nextId=1l;
+   // private List<Category> categories=new ArrayList<>();
+//    private Long nextId=1l;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public List<Category> getCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
-        categories.add(category);
+//        category.setCategoryId(nextId++);
+        categoryRepository.save(category);
 
     }
     @Override
     public String deleteCategory(Long categoryId) {
-        Category category=categories.stream()
-                .filter(c->c.getCategoryId()
-                        .equals(categoryId))
-                .findFirst().orElseThrow(()-> new ResponseStatusException (HttpStatus.NOT_FOUND,"Resource Not Found"));
-
-        categories.remove(category);
-        return "Category deleted successfully";
-
+        Category category=categoryRepository.findById(categoryId)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Category not found"));
+       categoryRepository.delete(category);
+       return "Category deleted successfully";
 
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
-        Optional<Category> optionalCategory=categories.stream()
-                .filter(c->c.getCategoryId().equals(categoryId))
-                .findFirst();
-        if(optionalCategory.isPresent()){
-            Category existingCategory=optionalCategory.get();
-            existingCategory.setCategoryName(category.getCategoryName());
-            return existingCategory;
-        }else {
-            throw new ResponseStatusException (HttpStatus.NOT_FOUND,"Category Not Found");
-        }
+        Category savedCategory=categoryRepository.findById(categoryId)
+                .orElseThrow(()-> new ResponseStatusException (HttpStatus.NOT_FOUND,"Category Not Found"));
+        category.setCategoryId(categoryId);
+        savedCategory=categoryRepository.save(category);
+        return savedCategory;
+
     }
 
 
