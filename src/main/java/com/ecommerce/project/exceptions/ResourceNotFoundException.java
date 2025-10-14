d7258ac32a484fc82f072fe7ee8c6a2c65d47fb0
Ansh <saxenaansh27@gmail.com>
2025-10-14 09:57:57 +0530

    Added for Handeling Reasource Not Found Exceptions

---
+++ src/main/java/com/ecommerce/project/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,29 @@
+package com.ecommerce.project.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+        public String resourceName;
+        public String field;
+
+    public String fieldName;
+       public Long  fieldId;
+
+    public ResourceNotFoundException(String resourceName, String field, String fieldName) {
+        super(String.format("%s not found with name %s : %s", resourceName, field,fieldName));
+        this.resourceName = resourceName;
+        this.field = field;
+        this.fieldName = fieldName;
+    }
+
+    public ResourceNotFoundException(String resourceName, String field, Long fieldId) {
+        super(String.format("%s not found with name %s : %d", resourceName, field,fieldId));
+        this.resourceName = resourceName;
+        this.field = field;
+        this.fieldId = fieldId;
+    }
+
+    public ResourceNotFoundException()
+    {
+
+    }
+}
