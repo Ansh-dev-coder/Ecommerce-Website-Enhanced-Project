55d82fa0d09bab56b2eda7bc138bd5735b7106e6
Ansh <saxenaansh27@gmail.com>
2025-10-30 17:10:08 +0530

    updated list to page

--- src/main/java/com/ecommerce/project/repositories/ProductRepository.java
+++ src/main/java/com/ecommerce/project/repositories/ProductRepository.java
@@ -2,19 +2,17 @@ package com.ecommerce.project.repositories;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.model.Product;
-import com.ecommerce.project.payload.ProductDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long>
 {
 
-    List<Product> findByCategoryOrderByPriceAsc(Category category);
+    Page<Product> findByCategoryOrderByPriceAsc(Category category, Pageable pageDetails);
 
-    List<Product> findByProductNameLikeIgnoreCase(String keyword);
+    Page<Product> findByProductNameLikeIgnoreCase(String keyword, Pageable pageDetails);
 }
 
