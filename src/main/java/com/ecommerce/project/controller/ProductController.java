a37921ac1dc91ff68155cebd355e33a633577f82
Ansh <saxenaansh27@gmail.com>
2025-10-30 10:20:21 +0530

    Added Product Controller

---
+++ src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -0,0 +1,63 @@
+package com.ecommerce.project.controller;
+
+
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.payload.ProductResponse;
+import com.ecommerce.project.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("admin/categories/{categoryId}/product")
+    public ResponseEntity<ProductDTO> addProduct(@Valid @RequestBody ProductDTO productDTO,
+                                                 @PathVariable Long categoryId) {
+        ProductDTO addProduct = productService.addProduct(categoryId, productDTO);
+        return new ResponseEntity<>(addProduct, HttpStatus.CREATED);
+    }
+    @GetMapping("public/products")
+    public ResponseEntity<ProductResponse> getAllProducts() {
+        ProductResponse productResponse = productService.getAllProducts();
+        return new ResponseEntity<>(productResponse, HttpStatus.OK);
+    }
+    @GetMapping("public/categories/{categoryId}/products")
+    public ResponseEntity<ProductResponse> getProductsByCategory(@PathVariable Long categoryId)
+    {
+        ProductResponse productResponse=productService.searchByCategories(categoryId);
+        return new ResponseEntity<>(productResponse, HttpStatus.OK);
+    }
+    @GetMapping("public/products/keyword/{keyword}")
+    public ResponseEntity<ProductResponse> getProductByKeyword(@PathVariable String keyword)
+    {
+        ProductResponse productResponse=productService.getProductByKeyword(keyword);
+        return new ResponseEntity<>(productResponse,HttpStatus.FOUND);
+    }
+    @PutMapping("admin/products/{productId}")
+    public ResponseEntity<ProductDTO> updateProduct(@Valid @RequestBody ProductDTO productDTO,@PathVariable Long productId)
+    {
+        ProductDTO updateProduct=productService.updateProduct(productId,productDTO);
+        return new ResponseEntity<>(updateProduct,HttpStatus.OK);
+    }
+    @PutMapping("products/{productId}/image")
+    public ResponseEntity<ProductDTO> updateProductImage(@PathVariable Long productId,
+                                                         @RequestParam("image") MultipartFile image) throws IOException {
+        ProductDTO updateProduct=productService.updateProductImage(productId,image);
+        return new ResponseEntity<>(updateProduct, HttpStatus.OK);
+    }
+    @DeleteMapping("admin/products/{productId}")
+    public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long productId)
+    {
+        ProductDTO deleteProduct=productService.deleteProduct(productId);
+        return  new ResponseEntity<>(deleteProduct,HttpStatus.OK);
+    }
+}
