735ec4a98f2e1ad12ae55f1795473b2c9ba978c3
Ansh <saxenaansh27@gmail.com>
2025-11-18 16:54:00 +0530

    Added User relationship

--- src/main/java/com/ecommerce/project/model/Product.java
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -31,4 +31,7 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+    @ManyToOne()
+    @JoinColumn(name = "seller_id")
+    private User user;
 }
