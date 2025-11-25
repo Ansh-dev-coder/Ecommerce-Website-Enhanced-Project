c4ae89b06ea4c103426bd7874bfd459ef4c4021b
Ansh <saxenaansh27@gmail.com>
2025-11-25 08:53:36 +0530

    Custom UserDetailsServiceImpl class

---
+++ src/main/java/com/ecommerce/project/security/jwt/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.ecommerce.project.security.jwt.security.services;
+
+import com.ecommerce.project.model.User;
+import com.ecommerce.project.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+      @Autowired
+      UserRepository userRepository;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       User user=userRepository.findByUsername(username).orElseThrow(()-> new UsernameNotFoundException("Username not found : %s"+username));
+          return      UserDetailImpl.build(user);
+    }
+}
