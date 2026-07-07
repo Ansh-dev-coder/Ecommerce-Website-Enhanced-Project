57b9c748f1be49611c4c3c3aed4f4ee7e3cce674
Ansh <saxenaansh27@gmail.com>
2026-07-07 13:37:33 +0530

    feat: implement PaymentMethod component for payment selection in Checkout

--- frontend/src/Checkout/PaymentMethod.jsx
+++ frontend/src/Checkout/PaymentMethod.jsx
@@ -1,8 +1,41 @@
+import React from 'react'
+import {useSelector} from 'react-redux'
+import {FormControl,FormControlLabel,RadioGroup,Radio} from '@mui/material'
+import {useDispatch} from 'react-redux'
+import {addPaymentMethod} from '../store/actions'
+
 const PaymentMethod=()=>{
+    const dispatch=useDispatch()
+
+    const {paymentMethod}=useSelector((state)=>state.payment)
+
+const paymentMethodHandler=(method)=>{
+    dispatch(addPaymentMethod(method))
+}
 
     return (
-        <div>
-            Hello Payment Method
+        <div >
+            <h1>Select Payment Method</h1>
+            <FormControl>
+                <RadioGroup
+                    aria-label="Payment Method"
+                    name="paymentMethod"
+                    value={paymentMethod}
+                    onChange={(e)=>paymentMethodHandler(e.target.value)}
+                >
+                    <FormControlLabel 
+                          value="Stripe" 
+                          control={<Radio color="primary"/>}
+                           label="Stripe"
+                           className="text-gray-700"/>
+
+                    <FormControlLabel
+                           value="Paypal" 
+                           control={<Radio color="primary" />} 
+                           label="Paypal"
+                           className="text-gray-700"/>
+                </RadioGroup> 
+            </FormControl>
         </div>
     )
 }
