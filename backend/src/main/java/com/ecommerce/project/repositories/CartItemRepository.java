5dbde8eb2f801f17ee1374636a128e7d84066a9a
Ansh <saxenaansh27@gmail.com>
2026-07-09 13:26:57 +0530

    feat: add deleteAllByCartId method to CartItemRepository for bulk deletion of cart items

--- backend/src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
+++ backend/src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
@@ -1,11 +1,14 @@
 package com.ecommerce.project.repositories;
 
+import com.ecommerce.project.model.Cart;
 import com.ecommerce.project.model.CartItem;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
@@ -17,4 +20,7 @@ public interface CartItemRepository extends JpaRepository<CartItem,Long> {
     @Transactional
     @Query("DELETE FROM CartItem ci WHERE ci.cart.id=?1 AND ci.product.id=?2")
     void deleteCartItemByProductIdAndCartId(Long cartId, Long productId);
+
+    @Query("DELETE FROM CartItem ci WHERE ci.cart.id=?1")
+    List<CartItem> deleteAllByCartId(Long cartId);
 }
