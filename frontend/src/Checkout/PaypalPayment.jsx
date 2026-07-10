66b8108cd49ec3f969b7742ac3a7492ec2eb9352
Ansh <saxenaansh27@gmail.com>
2026-07-10 17:56:12 +0530

    feat: update PaypalPayment component to display unavailability alert

--- frontend/src/Checkout/PaypalPayment.jsx
+++ frontend/src/Checkout/PaypalPayment.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Alert,AlertTitle } from '@mui/material'
 
 const PaypalPayment = () => {
   return (
-    <div>PaypalPayment</div>
+    <div  className='h-96 flex justify-center items-center'>
+        <Alert severity="warning"  variant='filled' style={{maxWidth : "400px"}} >
+  <AlertTitle>Paypal Unavailable</AlertTitle>
+  Paypal payment is unavailable. Please use another payment method.
+</Alert>
+    </div>
   )
 }
 
