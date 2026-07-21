90bdd425bfcaef107f29faffe3ba57d11dbac608
Ansh <saxenaansh27@gmail.com>
2026-07-21 18:28:56 +0530

    feat: refactor StripeServiceImpl to initialize Stripe API key and implement paymentIntent method

--- backend/src/main/java/com/ecommerce/project/service/StripeServiceImpl.java
+++ backend/src/main/java/com/ecommerce/project/service/StripeServiceImpl.java
@@ -1,20 +1,39 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.payload.StripePaymentDTO;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class StripeServiceImpl implements StripeService{
 
-    @Value("${STRIPE_SECRET_KEY}")
+    @Value("${stripe.secret.key}")
     private String StripeApiKey;
 
-    public PaymentIntent paymentIntent(StripePaymentDTO stripePaymentDTO){
+    @PostConstruct
+    public void init(){
+        Stripe.apiKey=StripeApiKey;
+    }
 
+    public PaymentIntent paymentIntent(StripePaymentDTO stripePaymentDTO) throws StripeException {
+        PaymentIntentCreateParams params=PaymentIntentCreateParams.builder()
+                .setAmount(stripePaymentDTO.getAmount())
+                .setCurrency(stripePaymentDTO.getCurrency())
+                .setAutomaticPaymentMethods(
+                        PaymentIntentCreateParams.AutomaticPaymentMethods.builder()
+                                .setEnabled(true)
+                                .build()
+                ).build();
 
-        return null;
+           PaymentIntent paymentIntent=PaymentIntent.create(params);
+        return paymentIntent;
     }
 
 
