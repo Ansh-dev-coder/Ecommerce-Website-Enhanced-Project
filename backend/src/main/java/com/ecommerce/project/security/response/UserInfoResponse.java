0d51de994d54a25aea71ca613c32b533c07e8e6d
Ansh <saxenaansh27@gmail.com>
2026-07-30 10:06:15 +0530

    feat: add email field to UserInfoResponse for enhanced user details

--- backend/src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
+++ backend/src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
@@ -12,6 +12,7 @@ public class UserInfoResponse {
     private Long id;
     private String jwtCookie;
     private String username;
+    private String email;
     private List<String> roles;
 
 //    public UserInfoResponse(Long id, String username, List<String> roles) {
