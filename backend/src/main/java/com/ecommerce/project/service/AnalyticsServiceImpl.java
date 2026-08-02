654ff4356ec650fc2ee25ba92b54f044b756aa82
Ansh <saxenaansh27@gmail.com>
2026-08-02 10:14:02 +0530

    feat: implement AnalyticsServiceImpl for retrieving analytics data

---
+++ backend/src/main/java/com/ecommerce/project/service/AnalyticsServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.payload.AnalyticsResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnalyticsServiceImpl implements AnalyticsService {
+
+    @Override
+    public AnalyticsResponse getAnalyticsData() {
+        return null;
+    }
+}
