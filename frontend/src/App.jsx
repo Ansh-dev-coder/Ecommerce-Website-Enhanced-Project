4197b1da796c2a81ef3e89ea21534db69693f44e
Ansh <saxenaansh27@gmail.com>
2026-08-02 09:16:37 +0530

    feat: restrict admin route access to admin users only

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -40,7 +40,7 @@ function App() {
               <Route path='/register' element={<Register />} />   
           </Route>
 
-          <Route path='/' element={<PrivateRoute/>}>
+          <Route path='/' element={<PrivateRoute  adminOnly />}>
             <Route path='/admin' element={<AdminLayout/>}/>
 
           </Route>
