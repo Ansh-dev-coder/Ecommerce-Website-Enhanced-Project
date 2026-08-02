1a585cd7be4e48ab24b6b720c6e04f139f23b96f
Ansh <saxenaansh27@gmail.com>
2026-08-02 11:25:20 +0530

    feat: refactor AnalyticsController to use AnalyticsService interface

--- backend/src/main/java/com/ecommerce/project/controller/AnalyticsController.java
+++ backend/src/main/java/com/ecommerce/project/controller/AnalyticsController.java
@@ -2,7 +2,9 @@ package com.ecommerce.project.controller;
 
 
 import com.ecommerce.project.payload.AnalyticsResponse;
+import com.ecommerce.project.service.AnalyticsService;
 import com.ecommerce.project.service.AnalyticsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class AnalyticsController {
 
-    private AnalyticsServiceImpl analyticsService;
+    @Autowired
+    private AnalyticsService analyticsService;
 
     @GetMapping("admin/app/analytics")
     public ResponseEntity<AnalyticsResponse> getAnalytics(){
         AnalyticsResponse response= analyticsService.getAnalyticsData();
+        System.out.println("RESPONSE DATA"+response);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
