50b15fec48c68d69e8a424c523f58aea39331ac8
Ansh <saxenaansh27@gmail.com>
2026-08-02 11:26:02 +0530

    feat: add missing import and annotation for AnalyticsService interface

--- backend/src/main/java/com/ecommerce/project/service/AnalyticsService.java
+++ backend/src/main/java/com/ecommerce/project/service/AnalyticsService.java
@@ -1,9 +1,9 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.payload.AnalyticsResponse;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface AnalyticsService {
-
-
     AnalyticsResponse getAnalyticsData();
 }
