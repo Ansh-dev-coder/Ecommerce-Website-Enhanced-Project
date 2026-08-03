ae71296b5235328e4eb3898f1d2f6ee732862faa
Ansh <saxenaansh27@gmail.com>
2026-08-03 11:44:20 +0530

    feat: add AdminReducer for managing analytics state and integrate into store

---
+++ frontend/src/store/reducers/AdminReducer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const initialState={
+    analytics:{}
+}
+
+export  const AdminReducer = (state = initialState,action) => {
+  switch(action.type){
+    case  "FETCH_ANALYTICS" :
+      return{
+        ...state,
+        analytics : action.payload
+      }
+      default:
+        return state;
+  }
+}
+
