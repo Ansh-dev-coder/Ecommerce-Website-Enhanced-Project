4faf902da4da2dba36b0b1a754cb064e4dc89d28
Ansh <saxenaansh27@gmail.com>
2026-08-05 10:02:03 +0530

    feat: implement pagination and sorting in getAllOrders method

--- backend/src/main/java/com/ecommerce/project/service/OrderServiceImpl.java
+++ backend/src/main/java/com/ecommerce/project/service/OrderServiceImpl.java
@@ -5,10 +5,15 @@ import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.*;
 import com.ecommerce.project.payload.OrderDTO;
 import com.ecommerce.project.payload.OrderItemDTO;
+import com.ecommerce.project.payload.OrderResponse;
 import com.ecommerce.project.repositories.*;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -103,5 +108,27 @@ public class OrderServiceImpl implements OrderService {
                        .add(mapper.map(item,OrderItemDTO.class)));
        orderDTO.setAddressId(addressId);
         return orderDTO;
+    }
+
+    @Override
+    public OrderResponse getAllOrders(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder) {
+
+        Sort sortByAndOrder=sortOrder.equalsIgnoreCase("asc")? Sort.by(sortBy).ascending() : Sort.by("dsc");
+        Pageable pageDetails= PageRequest.of(pageNumber,pageSize,sortByAndOrder);
+      Page<Order> pageOrders= orderRepository.findAll(pageDetails);
+      List<Order> orders=pageOrders.getContent();
+
+      List<OrderDTO> orderDTOS=orders.stream().map(
+              order -> mapper.map(order,OrderDTO.class)
+      ).toList();
+
+      OrderResponse orderResponse=new OrderResponse();
+      orderResponse.setContent(orderDTOS);
+      orderResponse.setPageNumber(pageOrders.getNumber());
+      orderResponse.setPageSize(pageOrders.getSize());
+      orderResponse.setTotalElements(pageOrders.getTotalElements());
+      orderResponse.setTotalPages(pageOrders.getTotalPages());
+      orderResponse.setLastPage(pageOrders.isLast());
+        return orderResponse;
     }
 }
