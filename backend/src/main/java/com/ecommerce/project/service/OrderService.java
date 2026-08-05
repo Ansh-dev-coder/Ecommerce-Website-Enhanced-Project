83c78cab358d38fd1404bc0a2bffb6fd682a27f9
Ansh <saxenaansh27@gmail.com>
2026-08-05 10:01:54 +0530

    feat: add transactional annotation to placeOrder method and enhance getAllOrders method signature

--- backend/src/main/java/com/ecommerce/project/service/OrderService.java
+++ backend/src/main/java/com/ecommerce/project/service/OrderService.java
@@ -1,9 +1,16 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.payload.OrderDTO;
+import com.ecommerce.project.payload.OrderResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface OrderService {
+    @Transactional
     OrderDTO placeOrder(String email, Long addressId, String paymentMethod, String pgName, String pgPaymentId, String pgStatus, String pgResponseMessage);
+
+
+    OrderResponse getAllOrders(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 }
