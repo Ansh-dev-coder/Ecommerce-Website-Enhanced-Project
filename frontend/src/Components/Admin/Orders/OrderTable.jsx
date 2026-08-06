6ce7eab1d2cacae9a3ed309f1cb45aa08eae5227
Ansh <saxenaansh27@gmail.com>
2026-08-06 10:08:29 +0530

    feat: refactor OrderTable component to use external columns definition

--- frontend/src/Components/Admin/Orders/OrderTable.jsx
+++ frontend/src/Components/Admin/Orders/OrderTable.jsx
@@ -1,91 +1,11 @@
 import { DataGrid } from '@mui/x-data-grid'
 import React from 'react'
 import { FaEdit } from 'react-icons/fa';
+import { columns } from '../../helper/tableColumn';
 
 
 const OrderTable = ({adminOrders,pagination}) => {
-    const columns = [
-  {
-    sortable : false,
-    disableColumnMenu : true,
-    field : "id",
-    headerName : "orderId",
-    minWidth : 180,
-    headerAlign: "center",
-    editable : false,
-    headerClassName : "test-black font-semibold border",
-    cellClassName : "test-slate-700 font-normal border",
-    renderHeader : (params) => <span className='text-center'>Order ID</span>
-  },
-  {
    
-    field : "email",
-    headerName : "Email",
-    minWidth : 180,
-    headerAlign: "center",
-    editable : false,
-    headerClassName : "test-black font-semibold border",
-    cellClassName : "test-slate-700 font-normal border",
-    renderHeader : (params) => <span className='text-center'>Email</span>
-   
-  },
-  {
-    sortable:true,
-    field : "totalAmount",
-    headerName : "Total Amount",
-    minWidth : 180,
-    headerAlign: "center",
-    editable : false,
-    headerClassName : "test-black font-semibold border",
-    cellClassName : "test-slate-700 font-normal border",
-    renderHeader : (params) => <span className='text-center'>Total Amount</span>
-  },
-  {
-    sortable:false,
-    field : "status",
-    headerName : "Status",
-    minWidth : 180,
-    headerAlign: "center",
-    editable : false,
-    headerClassName : "test-black font-semibold border",
-    cellClassName : "test-slate-700 font-normal border",
-    renderHeader : (params) => <span className='text-center'>Status</span>
-  },
-  {
-    sortable : false,
-    field : "date",
-    headerName : "Order Date",
-    minWidth : 180,
-    headerAlign: "center",
-    editable : false,
-    headerClassName : "test-black font-semibold border",
-    cellClassName : "test-slate-700 font-normal border",
-    renderHeader : (params) => <span className='text-center'>Order Date</span>
-  },
-  {
-    sortable : false,
-    field : "action",
-    headerName : "Actiond",
-    minWidth : 250,
-    headerAlign: "center",
-    editable : false,
-    headerClassName : "test-black font-semibold border",
-    cellClassName : "test-slate-700 font-normal border",
-    renderHeader : (params) => <span className='text-center'>Action</span>,
-    renderCell : (params)=>{
-        return (
-            <div className='flex justify-center items-center space-x-2 h-full pt-2'>
-                <button className='flex items-center bg-blue-500 text-white px-4 h-9 rounded-md' >
-                    <FaEdit  />
-
-
-                </button>
-            </div>
-        )
-    }
-  }
-];
-
 const tableRecords=adminOrders?.map((item)=>{
     return {
         id:item.orderId,
