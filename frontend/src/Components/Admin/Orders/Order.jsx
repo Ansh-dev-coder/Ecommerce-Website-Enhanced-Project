0022889aa1b1f4dce6bd3f69f4c72821de15ebce
Ansh <saxenaansh27@gmail.com>
2026-08-06 08:36:38 +0530

    feat: create Order component for handling order display logic

---
+++ frontend/src/Components/Admin/Orders/Order.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Order = () => {
+    const emptyOrder=true;
+  return (
+    <div>{emptyOrder ? (<div>
+
+    </div>
+
+    ):
+    (<div>
+        
+    </div>)}
+    
+    </div>
+  )
+}
+
+export default Order
