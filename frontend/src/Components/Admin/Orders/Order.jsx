37f28b367fc325749b4e1875d450c133d642aede
Ansh <saxenaansh27@gmail.com>
2026-08-13 11:50:29 +0530

    feat: refactor Order component to use Redux state for orders and pagination

--- frontend/src/Components/Admin/Orders/Order.jsx
+++ frontend/src/Components/Admin/Orders/Order.jsx
@@ -1,65 +1,15 @@
 import React from 'react'
 import { FaShoppingCart } from 'react-icons/fa';
 import OrderTable from './OrderTable';
+import { useSelector } from 'react-redux';
+import useOrderFilter from '../../../hooks/UseOrderFIlter';
 
 const Order = () => {
-    const adminOrders=[ {
-            "orderId": 5,
-            "email": "saxenaansh27@gmail.com",
-            "orderItemDTO": [],
-            "date": "2026-07-30",
-            "payment": {
-                "paymentId": 34,
-                "paymentMethod": "Online",
-                "pgPaymentId": "pi_3TylZNC8BvRNHWuV2ki9yyrh",
-                "pgStatus": "succeed",
-                "pgResponse": "Payment successful",
-                "pgName": "Stripe"
-            },
-            "totalPrice": 4499.0,
-            "orderStatus": "Order Accepted",
-            "addressId": 7
-        },
-        {
-            "orderId": 1,
-            "email": "saxenaansh27@gmail.com",
-            "orderItemDTO": [],
-            "date": "2026-07-28",
-            "payment": {
-                "paymentId": 30,
-                "paymentMethod": "Online",
-                "pgPaymentId": "pi_3Ty3sEC8BvRNHWuV2xPRvoyV",
-                "pgStatus": "succeed",
-                "pgResponse": "Payment successful",
-                "pgName": "Stripe"
-            },
-            "totalPrice": 102297.0,
-            "orderStatus": "Order Accepted",
-            "addressId": 10
-        },
-        {
-            "orderId": 4,
-            "email": "saxenaansh27@gmail.com",
-            "orderItemDTO": [],
-            "date": "2026-07-28",
-            "payment": {
-                "paymentId": 33,
-                "paymentMethod": "Online",
-                "pgPaymentId": "pi_3Ty3wqC8BvRNHWuV0Sl9M9CW",
-                "pgStatus": "succeed",
-                "pgResponse": "Payment successful",
-                "pgName": "Stripe"
-            },
-            "totalPrice": 102297.0,
-            "orderStatus": "Order Accepted",
-            "addressId": 10
-        }]
-    const pagination={ pageNumber: 0,
-    pageSize: 10,
-    totalElements: 3,
-    totalPages: 1,
-    lastPage: true}
-    const emptyOrder=!adminOrders || adminOrders?.length===0;
+    const {adminOrder,pagination}=useSelector((state)=>state.order)
+   
+useOrderFilter()
+
+    const emptyOrder=!adminOrder || adminOrder?.length===0;
   return (
     <div>{emptyOrder ? (
     <div>
@@ -71,7 +21,7 @@ const Order = () => {
 
     ):
     (<div>
-        <OrderTable  adminOrders={adminOrders} pagination={pagination} />
+        <OrderTable  adminOrders={adminOrder} pagination={pagination} />
 
     </div>)}
     
