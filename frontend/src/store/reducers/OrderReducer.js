dae2029810ed129abc39ccef92c5659d11c51bd2
Ansh <saxenaansh27@gmail.com>
2026-08-13 11:50:54 +0530

    feat: implement orderReducer to handle GET_ADMIN_ORDERS action and manage pagination state

--- frontend/src/store/reducers/OrderReducer.js
+++ frontend/src/store/reducers/OrderReducer.js
@@ -3,5 +3,22 @@ const initialState={
     pagination : {}
 }
 export const orderReducer=(state = initialState,action) =>{
-    return state;
+
+    switch(action.type){
+        case "GET_ADMIN_ORDERS" :
+            return {
+                ...state,
+                adminOrder : action.payload,
+                pagination : {
+                    ...state.pagination,
+                    pageNumber : action.pageNumber,
+                    pageSize: action.pageSize,
+                    totalElements : action.totalElements,
+                    totalPages: action.totalPages,
+                    lastPage : action.lastPage
+                }
+            }
+            default:
+                return state
+    }
 }
