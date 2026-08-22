e52d36a5f65c8a703f77834bb2534f05d2a8391f
Ansh <saxenaansh27@gmail.com>
2026-08-22 12:23:11 +0530

    "feat(user): add repository query to fetch users by role"

--- backend/src/main/java/com/ecommerce/project/repositories/UserRepository.java
+++ backend/src/main/java/com/ecommerce/project/repositories/UserRepository.java
@@ -1,7 +1,12 @@
 package com.ecommerce.project.repositories;
 
+import com.ecommerce.project.model.AppRole;
 import com.ecommerce.project.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -15,4 +20,8 @@ public interface UserRepository extends JpaRepository<User,Long> {
     boolean existsByUsername( String username);
 
     boolean existsByEmail(String email);
+
+
+    @Query("SELECT u FROM USERS u JOIN u.roles r WHERE r.roleName=:role")
+    Page<User> findByRoleName(@Param("role") AppRole appRole, Pageable pageDetails);
 }
