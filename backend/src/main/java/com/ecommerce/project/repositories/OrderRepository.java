2c0948f19572ef0aee83ee6ac3eb6755a79ed9ba
Ansh <saxenaansh27@gmail.com>
2026-08-23 17:05:13 +0530

    feat(order): add repository method to fetch orders by user

--- backend/src/main/java/com/ecommerce/project/repositories/OrderRepository.java
+++ backend/src/main/java/com/ecommerce/project/repositories/OrderRepository.java
@@ -2,12 +2,23 @@ package com.ecommerce.project.repositories;
 
 
 import com.ecommerce.project.model.Order;
+import com.ecommerce.project.model.User;
+import com.ecommerce.project.payload.OrderResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
     @Query("SELECT COALESCE(SUM(o.totalPrice),0)FROM Order o")
     Double getTotalRevenue();
+
+    Page<Order> findByUser(User user, Pageable pageDetails);
+
+
+//    List<Order> findByUser(User user);
 }
