32863a417b68838a6100a10f0456bc810291cfe1
Ansh <saxenaansh27@gmail.com>
2026-08-23 17:02:27 +0530

    feat(order): add user relationship to Order entity

--- backend/src/main/java/com/ecommerce/project/model/Order.java
+++ backend/src/main/java/com/ecommerce/project/model/Order.java
@@ -36,4 +36,7 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "address_id")
     private Address address;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
